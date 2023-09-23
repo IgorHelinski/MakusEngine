799a568760f0ed091819516551259189b50413f4
Zirael <igor.www.pl.com@gmail.com>
2023-09-23 14:28:56 +0200

    does not work

--- Engine/src/Graphics.cpp
+++ Engine/src/Graphics.cpp
@@ -1,5 +1,6 @@
 #include <Graphics.h>
 #include <glad/glad.h>
+#include <glad/glad_wgl.h>
 #include <GLFW/include/glfw3.h>
 #include <iostream>
 
@@ -18,18 +19,27 @@ void Graphics::InitGraphics()
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return;
 	}
+
+
+	// define the function's prototype
+	typedef void (*GL_GENBUFFERS) (GLsizei, GLuint*);
+	// find the function and assign it to a function pointer
+	GL_GENBUFFERS glGenBuffers = (GL_GENBUFFERS)wglGetProcAddress("glGenBuffers");
+	// function can now be called as normal
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
 	
 	glViewport(0, 0, 800, 600);
 }
 
-void Graphics::SetOrthographicProjection(float left, float right, float bottom, float top, float near, float far)
+void Graphics::SetOrthographicProjection(float left, float right, float bottom, float top, float nearr, float farr)
 {
 	// orthographic projcetion https://en.wikipedia.org/wiki/Orthographic_projection
 	float mat[4][4] = {
 		{2.0f / (right - left), 0.0f, 0.0f, 0.0f},
 		{0.0f, 2.0f / (top - bottom), 0.0f, 0.0f},
-		{0.0f, 0.0f, -2.0f / (far - near), 0.0f},
-		{-(right + left) / (right - left), -(top + bottom) / (top - bottom), -(far + near) / (far - near), 1.0f}
+		{0.0f, 0.0f, -2.0f / (farr - nearr), 0.0f},
+		{-(right + left) / (right - left), -(top + bottom) / (top - bottom), -(farr + nearr) / (farr - nearr), 1.0f}
 	};
 
 	glUniformMatrix4fv(glGetUniformLocation(NULL, "projection"), 1, GL_FALSE, &mat[0][0]);
--- Engine/src/MakusEngine.cpp
+++ Engine/src/MakusEngine.cpp
@@ -5,6 +5,10 @@
 #include <Graphics.h>
 #include <iostream>
 
+GLfloat pointVertex[] = { 1024 / 2, 512 / 2 };
+float playerX;
+float playerY;
+
 MakusEngine::MakusEngine()
 {
 }
@@ -17,12 +21,11 @@ void MakusEngine::StartMakus()
 {
 	Window window;
 	window.InitWindow();
-	window.CreateWindow(800, 600, "Makus Engine dev build");
+	window.CreateWindow(1024, 512, "Makus Engine dev build");
 
 	Graphics graphics;
 	graphics.InitGraphics();
-
-	graphics.SetOrthographicProjection(0, 800, 0, 600, 0.0f, 1.0f);
+	//graphics.SetOrthographicProjection(0, 1024, 0, 512, 0.0f, 1.0f);
 
 	OnCreate();
 
@@ -41,24 +44,29 @@ void MakusEngine::StartMakus()
 void MakusEngine::OnCreate()
 {
 	HelloWorld();
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0, 1024, 0, 768, 0, 1);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	//glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+	playerX = 300;
+	playerY = 300;
 }
 
 void MakusEngine::OnUpdate()
 {
-	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	float playerX = 10;
-	float playerY = 10;
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glPointSize(50);
+	glVertexPointer(2, GL_FLOAT, 0, pointVertex);
+	glDrawArrays(GL_POINT, 0, 1);
+	glDisableClientState(GL_VERTEX_ARRAY);
 
-	glColor3f(1, 1, 0);
-	glPointSize(8);
-	glBegin(GL_POINTS);
-	glVertex2i((GLint)playerX, (GLint)playerY);
-	glEnd();
-
-	playerX = 200;
-	playerY = 200;
 }
 
 void MakusEngine::OnQuit()
